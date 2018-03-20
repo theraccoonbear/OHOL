ce0cfca7d1c99a439035cf0b96e9507ca859ee95
Don Smith <don.c.smith@gmail.com>
2018-03-19 21:27:26 -0500

    mostly working

--- Dockerfile
+++ Dockerfile
@@ -3,10 +3,10 @@ FROM ubuntu:latest
 ADD ./ /opt/src
 WORKDIR /opt/src
 
-RUN apt-get update && apt-get install curl git g++ imagemagick xclip libsdl1.2-dev libglu1-mesa-dev libgl1-mesa-dev make libpng12-dev -y
+RUN apt-get update && apt-get install curl git g++ imagemagick xclip libsdl1.2-dev libglu1-mesa-dev libgl1-mesa-dev make libpng12-dev nano -y
 RUN ./docker/fetch-and-compile.sh
 EXPOSE 8005
- 
-#CMD sleep 60000
+EXPOSE 5077
 
-CMD /opt/src/OneHourOneLife/OneLife/server/OneLifeServer
+CMD sleep 10000
+#CMD /opt/OneHourOneLife/OneLife/server/OneLifeServer
--- docker/fetch-and-compile.sh
+++ docker/fetch-and-compile.sh
@@ -6,7 +6,7 @@ fi
 TICKET_ID=$(cat /opt/src/ticketID.cfg)
 
 if [ ! "$TICKET_ID" ]; then
-	echo "No TICKET_ID defined in docker-compose.yml or .env"
+	echo "No TICKET_ID defined in ticketID.cfg"
 	exit 2
 fi
 
@@ -21,8 +21,8 @@ fi
 
 echo Source: $SOURCE_URL
 
-mkdir -p /opt/src/OneHourOneLife
-cd /opt/src/OneHourOneLife
+mkdir -p /opt/OneHourOneLife
+cd /opt/OneHourOneLife
 
 curl -o source.tar.gz $SOURCE_URL
 
@@ -37,3 +37,6 @@ make
 ln -s ../../OneLifeData7/objects .
 ln -s ../../OneLifeData7/transitions .
 ln -s ../../OneLifeData7/categories .
+
+sed -i 's/1/0/' settings/useStatsServer.ini
+sed -i 's/1/0/' settings/requireTicketServerCheck.ini
