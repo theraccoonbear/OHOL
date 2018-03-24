7ae7fb1557b9803448a53caec3acc4b06b072e82
Don Smith <don.c.smith@gmail.com>
2018-03-24 18:33:30 -0500

    more things that are not working

--- docker/run-server.sh
+++ docker/run-server.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+if [ ! -d "/opt/src/data/settings" ]; then
+	mkdir -p /opt/src/data/settings
+	cp /opt/game/OneLife/server/settings/* /opt/src/data/settings/
+fi
+
 cd /opt/src/data
-./opt/game/OneLife/server/OneLifeServer
+/opt/game/OneLife/server/OneLifeServer
+
